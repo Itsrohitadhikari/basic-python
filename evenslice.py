a68706938c8d0ba17f1dfb95460a81491150f812
Rohit Adhikari <89678745+Itsrohitadhikari@users.noreply.github.com>
2024-09-15 17:55:46 +0545

    Create evenslice.py

---
+++ evenslice.py
@@ -0,0 +1,7 @@
+# Print characters present at an even index number
+# Write a Python code to accept a string from the user and display characters present at an even index number
+
+
+str = input("Enter a String: ")
+leng = len(str) + 1
+print(str[0:leng:2])
